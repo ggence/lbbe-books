02ad55f10d80d3fc143b8f9fcb892c71aed14a48
Simon Penel <simon.penel@univ-lyon1.fr>
2017-09-22 16:19:38 +0200

    Ajout de la fonction Rendre

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,33 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'LBBE-Books' });
 });
-router.get('/EmprunterRendre', function(req, res, next) {
-  res.render('EmprunterRendre', { title: 'Emprunter et Rendre' });
-});
+
+
+
+// router.get('/EmprunterRendre', function(req, res, next) {
+//   res.render('EmprunterRendre', { title: 'Emprunter et Rendre' });
+// });
+
+
+
 router.post('/Emprunter', function(requete, res, next) {
-  console.log("L'utilisateur "+ requete.body.emprunteur, "a emprunte" +requete.body.livre);
+  console.log("L'utilisateur "+ requete.body.emprunteur, "a emprunte " +requete.body.livre);
 
   // Ici une requete
-
+  //
   //une promesse qui fait la requete, qui renvoie sur une page bilan en cas de succes et vers une page d'erreur sinon
 
   res.render('EmpruntOK', { title:requete.body.emprunteur+ ' a emprunte '+requete.body.livre ,livre: requete.body.livre,emprunteur: requete.body.emprunteur });
+});
+
+router.post('/Rendre', function(requete, res, next) {
+  console.log("Le livre " +requete.body.livre + "est rendu");
+
+  // Ici une requete
+  //
+  //une promesse qui fait la requete, qui renvoie sur une page bilan en cas de succes et vers une page d'erreur sinon
+
+  res.render('RendreOK', {livre: requete.body.livre });
 });
 
 module.exports = router;
