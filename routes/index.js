1148f48387f43992c8557d520ac35b6ee922ef5f
bspataro <spat.news@free.fr>
2017-09-22 13:39:42 +0200

    construction factice d'un liste de livre + envoi à la vue

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+//var bdd = require('InteragitBDD');
+
+/* =============================================================
+partie bidon en attendant le developpement de l'API BDD */
+
+/* definition d'une classe livre */
+function Livre(id,isbn,titre,auteurs,id_proprietaire,emplacement,empruntStatus){
+    this.id = id;
+    this.isbn = isbn;
+    this.titre = titre;
+    this.auteurs = auteurs;
+    this.id_proprietaire = id_proprietaire;
+    this.emplacement = emplacement;
+    this.empruntStatus = empruntStatus;
+}
+
+/*instanciation de livre */
+var livre1 = new Livre(1,"132321321312","Toto à la plage", "Hugo V", 4, "dtc",false);
+var livre2 = new Livre(2,"98746468987","Toto à la montagne", "Zola E", 4, "dtc",true);
+var livre3 = new Livre(3,"1337","Toto à la femre de calcul", "Delomtte S", 12, "Omega",false);
+
+/* constructiuon d'un tableau de livres */
+var listeLivres=[];
+listeLivres.push(livre1,livre2,livre3);
+//console.log(listeLivres);
+
+// ============================================================== */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LBBE-Books' });
+  //var livres=bdd.getListeLivres();
+  res.render('index', { title: 'LBBE-Books', liste: listeLivres });
 });
 
 module.exports = router;
