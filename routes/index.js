e094aaf8e0fc726449030cf5667f17151bf31ce5
Lionel Humblot <lionel.humblot@univ-lyon1.fr>
2017-09-22 14:26:24 +0200

    Création de la route en get d'ajout d'un livre. Création du formulaire de saisie d'un livre.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'LBBE-Books' });
 });
 
+/* GET page ajouter livre */
+router.get('/ajouterLivre', function(req, res, next) {
+  res.render('ajouterLivre', { title: 'LBBE-Books ajouter un livre' });
+});
+
+
+
 module.exports = router;
