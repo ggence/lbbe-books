23ad596fca2d3d9d53befd4e28785bb1b449a0d4
Simon Penel <simon.penel@univ-lyon1.fr>
2017-09-22 15:33:22 +0200

    ajout commentaires

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,11 @@ router.get('/EmprunterRendre', function(req, res, next) {
 });
 router.post('/Emprunter', function(requete, res, next) {
   console.log("L'utilisateur "+ requete.body.emprunteur, "a emprunte" +requete.body.livre);
+
+  // Ici une requete
+
+  //une promesse qui fait la requete, qui renvoie sur une page bilan en cas de succes et vers une page d'erreur sinon
+
   res.render('EmpruntOK', { title:requete.body.emprunteur+ ' a emprunte '+requete.body.livre ,livre: requete.body.livre,emprunteur: requete.body.emprunteur });
 });
 
