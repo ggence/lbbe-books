bd0ddd7027c539f321c4929652c8fdebbea4c9b1
Simon Penel <simon.penel@univ-lyon1.fr>
2017-09-22 14:52:47 +0200

    Le formulaire est en mode POST

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ router.get('/', function(req, res, next) {
 router.get('/EmprunterRendre', function(req, res, next) {
   res.render('EmprunterRendre', { title: 'Emprunter et Rendre' });
 });
+router.post('/Emprunter', function(requete, res, next) {
+  console.log("L'utilisateur "+ requete.body.emprunteur, "a emprunte" +requete.body.livre);
+  res.render('index', { title: 'Emprunter '+requete.body.livre ,msg: requete.body.livre });
+});
 
 module.exports = router;
