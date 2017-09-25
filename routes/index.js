116554e0010a146880142e774e489ebfcdee19af
Lionel Humblot <lionel.humblot@univ-lyon1.fr>
2017-09-25 17:34:38 +0200

    Formulaire avec recherche apartir de l'isbn et preremplissage

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var isbn = require('node-isbn');
+
+//var interrogation_bdd = require('../lib/InteragitBDD');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,9 +11,22 @@ router.get('/', function(req, res, next) {
 
 /* GET page ajouter livre */
 router.get('/ajouterLivre', function(req, res, next) {
-  res.render('ajouterLivre', { title: 'LBBE-Books ajouter un livre' });
+  // recuperer ici les proprietaires ds la bdd (utilisation de la methode adequat du module interragitBDD  var proprietaires =  interrogation_bdd.getListUtilisateurs();
+  //var proprietaires =  interrogation_bdd.getListUtilisateurs();
+  //res.render('ajouterLivre', { title: 'LBBE-Books ajouter un livre', liste_proprietaires: proprietaires });
 });
 
-
+/* POST search ISBN*/
+router.post('/searchISBN', function(req, res, next) { 
+  var isbn_livre=req.body.isbn;
+  isbn.resolve(isbn_livre, function (err, book) {
+    // il faut ameliorer ici la gestion de l'erreur voir dans la donc node-isbn
+    if (err) {
+      res.status(444).json('Pas de livre trouvé avec cet ISBN');
+    } else {
+      res.json(book);
+    }
+  });
+});
 
 module.exports = router;
